25783c378cb7faefdc6dda9eb018731655de6daf
langamitch <langamitch69@gmail.com>
2025-07-15 10:32:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,12 +12,13 @@ import {
 // Your web app's Firebase configuration
 // PASTE YOUR FIREBASE CONFIGURATION OBJECT HERE
 const firebaseConfig = {
-  apiKey: "AIzaSy...",
-  authDomain: "your-project-id.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project-id.appspot.com",
-  messagingSenderId: "...",
-  appId: "1:..."
+  apiKey: "AIzaSyAaH8a4yrRhObrHInRqC5HdVB52-hKWdLU",
+  authDomain: "lucent-8cdaa.firebaseapp.com",
+  projectId: "lucent-8cdaa",
+  storageBucket: "lucent-8cdaa.firebasestorage.app",
+  messagingSenderId: "85128436861",
+  appId: "1:85128436861:web:dae41ae739b49c78c02514",
+  measurementId: "G-9N28JRSVEV"
 };
 
 // Initialize Firebase
