19a4e61a207ab179ac23c9b93cac643b19153881
langamitch <langamitch69@gmail.com>
2025-07-18 04:44:13 +0200

    Create App.js

---
+++ App.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAaH8a4yrRhObrHInRqC5HdVB52-hKWdLU",
+  authDomain: "lucent-8cdaa.firebaseapp.com",
+  projectId: "lucent-8cdaa",
+  storageBucket: "lucent-8cdaa.firebasestorage.app",
+  messagingSenderId: "85128436861",
+  appId: "1:85128436861:web:dae41ae739b49c78c02514",
+  measurementId: "G-9N28JRSVEV"
+};
